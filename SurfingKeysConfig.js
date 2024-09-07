754804a4a9ce2f4580eab71b8c35aed64fe6ae08
Jake Therien <Jake.Therien@pm.me>
2024-09-06 20:18:12 -0400

    Update

--- SurfingKeysConfig.js
+++ SurfingKeysConfig.js
@@ -11,32 +11,35 @@
 // api.mapkey("ou", "Open a URL", 'Normal.openOmnibar({type: "URLs"})');
 
 // unmap for reddit
-api.unmap('j', /old.reddit.com/);
-api.unmap('k', /old.reddit.com/);
-api.unmap('z', /old.reddit.com/);
+api.unmap("j", /old.reddit.com/);
+api.unmap("k", /old.reddit.com/);
+api.unmap("z", /old.reddit.com/);
 
 // unmap and remap search engines
-api.unmap('sg');
-api.unmap('sd');
-api.unmap('se');
-api.unmap('sw');
-api.unmap('ss');
-api.unmap('sh');
-api.unmap('sy');
+api.unmap("sg");
+api.unmap("sd");
+api.unmap("se");
+api.unmap("sw");
+api.unmap("ss");
+api.unmap("sh");
+api.unmap("sy");
 
-// api.addSearchAlias('b', 'brave', 'https://www.brave.com/search?q=');
-// api.addSearchAlias('r', 'subreddit', 'https://old.reddit.com/r/');
+// api.addSearchAlias("b", "brave", "https://www.brave.com/search?q=");
+// api.addSearchAlias("r", "subreddit", "https://old.reddit.com/r/");
 
-api.unmap('om');
-api.unmap('og');
-api.unmap('od');
-api.unmap('ob');
-api.unmap('oe');
-api.unmap('ow');
-api.unmap('os');
-api.unmap('oy');
-api.unmap('ox');
-api.unmap('oh');
+api.unmap("om");
+api.unmap("og");
+api.unmap("od");
+api.unmap("ob");
+api.unmap("oe");
+api.unmap("ow");
+api.unmap("os");
+api.unmap("oy");
+api.unmap("ox");
+api.unmap("oh");
+
+// follow links hotkeys
+api.Hints.setCharacters("qweasdzxc");
 
 // follow links hotkeys
 api.Hints.setCharacters('qweasdzxc');
